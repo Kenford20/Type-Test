305a16d3add1d587aece353db708569e540f6e72
Kenford <kzhou0066@gmail.com>
2018-07-29 22:48:25 -0400

    added running timer functionality

--- type/script.js
+++ type/script.js
@@ -4,11 +4,12 @@ window.onload=function() {
 
 // Global Variables
 // -------------------------------------------
-var timer = document.querySelector("#sub-button");
+var a = document.querySelector("#sub-button");
 var restartbtn = document.querySelector("#restart");
 var text = document.querySelector("#typing-box");
 var textbtn = document.querySelector("#new-text");
 var quoteurl = "https://talaikis.com/api/quotes/random/";
+var minutes = 0, seconds=0, ms = 0; // starting value for the timer
 
 // Function Defitions
 // ------------------------------------------------
@@ -23,21 +24,53 @@ function sendRequest() {
 	AJAXrequest.send();
 }
 
+// changes the test text whenever the "new text" button is clicked
 function renderHTML(data){
 	console.log(data)
 	document.querySelector("#quote-api").innerHTML = data.quote;
-	// add the quote from api stored in a var in place of "abcd"
 }
 
 function logTextInput() {
-	console.log(text);
 	console.log(text.value);
 }
 
+// adds the leading zeros to the timer
+function displayZero(time){
+	if(time <= 9) {
+		time = "0" + time;
+	}
+	return time;
+}
+
+// concatenates the different timer values with colons to display timer up to minutes
+function runTimer(){
+	document.querySelector(".timer").innerHTML= displayZero(minutes) + ":" + displayZero(seconds) + ":" + displayZero(ms);
+	ms++;
+
+	// time conversions
+	if(seconds == "60"){
+		seconds = "0";
+		minutes++;
+	}
+
+	if(ms === 100){
+		ms=0;
+		seconds++;
+	}
+}
+
+// checks when user begins typing, then calls runTimer to start the timer
+function startTimer(){
+	let textLength = text.value.length;
+	if(textLength === 0){
+		setInterval(runTimer, 10) // run function every hundred of a second
+	}
+}
 
 // main()
 // ----------------------------------------------------
 //restartbtn.addEventListener("click", , false);
 text.addEventListener("keyup", logTextInput, false);
-textbtn.addEventListener("click", sendRequest)
+text.addEventListener("keypress", startTimer, false);
+textbtn.addEventListener("click", sendRequest, false);
 }
